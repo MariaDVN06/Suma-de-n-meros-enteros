e1b7f45f5fb934f1d68a6d90b8575553bb5e54d3
MariaDVN06 <106028315+MariaDVN06@users.noreply.github.com>
2022-05-23 00:04:45 -0400

    Add files via upload

---
+++ main_4.js
@@ -0,0 +1,10 @@
+function combinar(){
+
+    var primeracas = parseInt(document.getElementById("cas1").value);
+    var segcas = parseInt(document.getElementById("cas2").value);
+
+    
+
+    console.log(primeracas + segcas);
+
+}
